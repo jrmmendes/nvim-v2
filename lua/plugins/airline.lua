33c6c333d5d0ee372a461f01fa8dae0f12fbf10f
mendes <jrmmendes@outlook.com>
2025-04-27 17:16:33 -0300

    improve settings for statusline, notifications and autosave

--- lua/plugins/airline.lua
+++ lua/plugins/airline.lua
@@ -5,30 +5,37 @@ return {
     priority=1000,
     dependencies = {
         'vim-airline/vim-airline-themes',
+        'tpope/vim-fugitive',
         'ryanoasis/vim-devicons'
     },
     config = function()
+        vim.g.airline_symbols = vim.g.airline_symbols or {}
         -- Airline configuration
         vim.g.airline_powerline_fonts = 1
         vim.g.Powerline_symbols = 'unicode'
         vim.g.gruvbox_contrast_dark = "hard"
-        vim.g.airline_theme="base16_gruvbox_dark_soft"
+        --vim.g.airline_theme="base16_gruvbox_dark_soft"
+        vim.g.airline_theme="minimalist"
 
         -- Tabline extensions
         vim.g['airline#extensions#tabline#enabled'] = 1
         vim.g['airline#extensions#tabline#show_tab_nr'] = 1
         vim.g['airline#extensions#tabline#tab_nr_type'] = 2
+        vim.g['airline#extensions#branch#enabled'] = 0
         vim.g['airline#extensions#tabline#show_tab_type'] = 1
         vim.g['airline#extensions#tabline#buffers_label'] = 'BUFFERS'
         vim.g['airline#extensions#tabline#tabs_label'] = 'TABS'
-
         -- Unicode symbols for separators
-        vim.g.airline_left_sep = ''
-        vim.g.airline_left_alt_sep = ''
-        vim.g.airline_right_sep = ''
-        vim.g.airline_right_alt_sep = ''
+        vim.g.airline_left_sep = ''
+        vim.g.airline_left_alt_sep = ''
+        vim.g.airline_right_sep = ''
+        vim.g.airline_right_alt_sep = ''
+        vim.g.airline_symbols.branch = '>'
 
-        -- Fugitive statusline integration
-        vim.g.airline_fugitive='%{FugitiveStatusline()}'
+
+        -- +-----------------------------------------------------------------------------+
+        -- | A | B |                     C                            X | Y | Z |  [...] |
+        -- +-----------------------------------------------------------------------------+
+        vim.g.airline_section_b = '%{FugitiveHead() == "" ? "" : " " . FugitiveHead()}'
     end
 }
--- lua/plugins/vim-auto-save.lua
+++ lua/plugins/vim-auto-save.lua
@@ -1,3 +1,6 @@
 return {
-    '907th/vim-auto-save'
+    '907th/vim-auto-save',
+    config = function()
+        vim.g.auto_save = 1 -- Enable auto-save by default
+    end
 }
